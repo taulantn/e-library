a1a213f5216f1b857aeea785fa6ec2c3b9f61115
Feston Ahmeti <fa47928@ubt-uni.net>
2023-05-21 14:27:54 +0200

    Added a Landing Page(Home Page)

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route  } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import SideBar from './Admin/components/Sidebar';
 import sidebar_menu from './Admin/constants/sidebar-menu';
@@ -10,27 +10,33 @@ import Books from './Admin/pages/Books';
 import Orders from './Admin/pages/Orders';
 import Categories from './Admin/pages/Category';
 import Users from './Admin/pages/Users';
+import Home from './Landing/pages/Home';
 
-function App () {
-  return(
-    <Router>
-      <div className='dashboard-container'>
-        <SideBar menu={sidebar_menu} />
-          
-          <div className='dashboard-body'>
-              <Routes>
-                  <Route path="*" element={<div></div>} />
-                  <Route exact path="/" element={<div></div>} />
-                  <Route exact path="/books" element={<Books/>} />
-                  <Route exact path="/categories" element={<Categories/>} />
-                  <Route exact path="/users" element={<Users/>} />
-                  <Route exact path="/orders" element={<Orders/>} />
-                  <Route exact path="/profile" element={<div></div>} />
-              </Routes>
-          </div>
+function Dashboard() {
+  return (
+    <div className='dashboard-container'>
+      <SideBar menu={sidebar_menu} />
+      <div className='dashboard-body'>
+        <Routes>
+          <Route path="/books" element={<Books />} />
+          <Route path="/categories" element={<Categories />} />
+          <Route path="/users" element={<Users />} />
+          <Route path="/orders" element={<Orders />} />
+        </Routes>
       </div>
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/*" element={<Dashboard />} />
+      </Routes>
     </Router>
-  )
+  );
 }
 
 export default App;
